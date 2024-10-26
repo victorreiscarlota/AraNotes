81bdb4c7c555f251573226a6008ebdd9f76364ff
victorreiscarlota <victor.reis.carlotta@gmail.com>
2024-10-26 17:42:13 -0300

    first commit

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import { ArrowRight } from "lucide-react"
+import { Heroes } from "./Heroes"
+import { useConvexAuth } from "convex/react"
+import { Spinner } from "@/components/spinner"
+import Link from "next/link"
+import { SignInButton } from "@clerk/clerk-react"
+
+	
+
+export default function Heading () {
+
+  const {isAuthenticated,isLoading} = useConvexAuth()
+
+return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">AraNotes</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        AraNotes is the connected workspace where <br/>
+      better, faster work happens</h3>
+      {isLoading && <div className="w-full flex justify-center items-center">
+        <Spinner size='lg'/>
+        </div>}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href='/documents'>
+            Enter AraNotes
+            <ArrowRight className="w-4 h-4 ml-2"/>
+          </Link>
+      </Button>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <SignInButton mode='modal'>
+            <Button>
+              Get AraNotes Free
+              <ArrowRight className="w-4 h-4 ml-2"/>
+            </Button>
+          </SignInButton>
+        )}
+    </div>
+)
+}
